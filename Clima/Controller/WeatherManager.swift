b32cdf35b7c8f889adca87ae9542b0abede92338
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-03-23 17:44:05 -0300

    add Switch statement

--- Clima/Controller/WeatherManager.swift
+++ Clima/Controller/WeatherManager.swift
@@ -37,9 +37,31 @@ struct WeatherManager {
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             let id = decodedData.weather[0].id
-            self.getConditonName()
+            print(getConditionName(weatherId: id))
         } catch {
             print(error)
         }
+    }
+    
+    func getConditionName(weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud"
+        default:
+            return "sun.min"
+        }
+        
     }
 }
